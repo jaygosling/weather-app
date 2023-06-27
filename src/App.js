ce582d09d522e4b972cf4775b943df0f25fb4135
jaygosling <javijimenezclemente@gmail.com>
2023-06-27 12:33:59 +0200

    Added weather cards and possibility to add many cities. Added close button, still WIP. Added sidebar.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './js/pages/Home'
 import ShowCity from './js/component/ShowCity';
 import injectContext from './js/store/appContext';
+import ShowWeather from './js/pages/ShowWeather';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/city/:id" element={<ShowCity/>} />
+        <Route path="/test" element={<ShowWeather/>} />
       </Routes>
     </BrowserRouter>
     
